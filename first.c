af6e23a092a63cdace2c6b3111560379a2177883
Shubham Kalihari <shubhamkalihari09@gmail.com>
2021-03-23 00:13:26 +0530

    2nd commit change in tasks.json

--- first.c
+++ first.c
@@ -1,6 +1,8 @@
 #include<stdio.h> // define the header file  
 int main()   // define the main function  
 {  
+    int x=10;
+    int y=20;
     printf("Hello world");  // print the statement.
     return 0;  
 }  
